491fd5182b18c184b67f4c77bf278b1b4ae84f70
l.forzutti <l.forzutti@egeatech.com>
2023-10-07 06:30:00 +0200

    chore api: GenerateApi

---
+++ misc/GenerateAppKey.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const path = require("path");
+
+let content;
+const uuidLength = Number(process.argv[2] ?? 120);
+console.log(`Opening file: ${path.resolve(__dirname + "/../.env")}`);
+
+const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+console.log(process.argv);
+
+fs.readFile(path.resolve(__dirname + "/../.env"), "utf-8", (err, data) => {
+    if (err) throw err;
+
+    content = data;
+
+    let contentLines = content.split("\n");
+
+    for (let i = 0; i < contentLines.length; i++) {
+        if (contentLines[i].includes("APP:KEY")) {
+            let res = "";
+            const chaLength = chars.length;
+
+            for (let i = 0; i < uuidLength; i++) {
+                res += chars.charAt(Math.floor(Math.random() * chaLength));
+            }
+
+            contentLines[i] = `APP_KEY=${res}`;
+        }
+    }
+
+    contentLines = contentLines.join("\n");
+
+    fs.writeFile(path.resolve(__dirname + "/../.env"), contentLines, {encoding: "utf-8", flag: "w"}, err => {
+        if (err) console.log(err);
+        else console.log("APP_KEY updated!");
+    });
+});
